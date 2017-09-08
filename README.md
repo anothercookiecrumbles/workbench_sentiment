c196a12a58e0d129f2ad1aea56fa1a756689a4b1
anothercookiecrumbles <priyanjana@mac.com>
2017-09-08 16:02:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+You'll need to ensure that the below python libraries are installed before you can use this module:
+
+nltk==3.2.4
+twython==3.6.0
---
+++ sentiment_analysis.py
@@ -0,0 +1,36 @@
+import nltk
+import pandas as pd
+
+@staticmethod
+def __init__(self):
+    pass
+
+@staticmethod
+def event():
+    pass
+
+def render(wf_module, table):
+    if table == None:
+        return None
+
+    column = wf_module.get_param_string('textcolumn')
+    if column == '' or column == None:
+        wf_module.set_ready(notify=False)
+        return None
+
+    if column not in table.columns:
+        wf_module.set_error("No column {} exists.".format(column))
+        return None
+
+    data = table[column]
+    sentiment = []
+
+    sid = SentimentIntensityAnalyzer()
+
+    for text in data:
+        score = sid.polarity_scores(text)
+        sentiment.append(score['compound'])
+
+    # add column to existing table
+    table = table.assign(sentiment=pd.Series(sentiment))
+    return table
