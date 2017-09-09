3c5f4029239db713c9650566e964e0546cf6a722
anothercookiecrumbles <priyanjana@mac.com>
2017-09-08 22:24:48 -0400

    Ugh, forgot to wrap functions in a class.

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,37 +1,39 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
 
-@staticmethod
-def __init__(self):
-    pass
+class SentimentAnalyser:
 
-@staticmethod
-def event():
-    pass
+    @staticmethod
+    def __init__(self):
+        pass
 
-@staticmethod
-def render(wf_module, table):
-    if table == None:
-        return None
+    @staticmethod
+    def event():
+        pass
 
-    column = wf_module.get_param_string('textcolumn')
-    if column == '' or column == None:
-        wf_module.set_ready(notify=False)
-        return None
+    @staticmethod
+    def render(wf_module, table):
+        if table == None:
+            return None
 
-    if column not in table.columns:
-        wf_module.set_error("No column {} exists.".format(column))
-        return None
+        column = wf_module.get_param_string('textcolumn')
+        if column == '' or column == None:
+            wf_module.set_ready(notify=False)
+            return None
 
-    all_texts = table[column]
-    sentiment = []
+        if column not in table.columns:
+            wf_module.set_error("No column {} exists.".format(column))
+            return None
 
-    sid = SentimentIntensityAnalyzer()
+        all_texts = table[column]
+        sentiment = []
 
-    for text in all_texts:
-        score = sid.polarity_scores(text)
-        sentiment.append(score['compound'])
+        sid = SentimentIntensityAnalyzer()
 
-    # add column to existing table
-    table = table.assign(sentiment=pd.Series(sentiment))
-    return table
+        for text in all_texts:
+            score = sid.polarity_scores(text)
+            sentiment.append(score['compound'])
+
+        # add column to existing table
+        table = table.assign(sentiment=pd.Series(sentiment))
+        return table
